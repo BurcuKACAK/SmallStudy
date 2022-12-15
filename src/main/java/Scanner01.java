ab11b37dd7e6c9d3269674eacd6a597ca56eb481
Burcu Kacak <burcu.kacakk@gmail.com>
2022-12-16 00:28:40 +0300

    version1.1

---
+++ src/main/java/Scanner01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Scanner01 {
+
+    public static void main(String[] args) {
+
+
+            Scanner input =new Scanner(System.in);
+            System.out.println("lutfen yasinize girniz");
+            int yas=input.nextInt();
+            System.out.println("yasiniz :"+ yas);
+
+
+
+    }
+}
