6b63e0624cc8eec83d68b193ba5a7e4de6fa02dd
Burcu Kacak <burcu.kacakk@gmail.com>
2022-12-21 20:46:07 +0300

    first commit

---
+++ src/main/java/StringManipulations/C01_StringManipulation.java
@@ -0,0 +1,12 @@
+package StringManipulations;
+
+public class StringManipulation01 {
+    public static void main(String[] args) {
+       // soru 1)Girilen iki String veriyi karşılaştıran Java kodu yazınız.
+
+        String m = "JAVA";
+        String n = "java";
+        Boolean x = m.equals(n);
+        System.out.println(x);
+    }
+}
